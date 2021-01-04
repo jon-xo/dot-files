8c3fa41b987cd8763ea10f7d50c8d943d93443a9
jonmacchio <robotlove@gmail.com>
2021-01-04 15:40:44 -0500

    Added instructions to setup a new cron job.

--- dot-back.sh
+++ dot-back.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+# change directory to home folder
 cd ~
+
+# list file names and run grep search which includes "bash, fzf, zsh" and excludes filenames with "history, swp, pre". Copy grep results to working directory.
 ls -1a | grep -i 'bash\|fzf\|zsh' | grep -v 'history\|swp\|pre' | xargs -I '{}' cp -Rf '{}' ~/bin/dot-files/
+
+# Create conjob
+## $ crontab -e
+# Execute cron job every 15 minutes
+## */15 * * * * ~/bin/dot-files/dot-back.sh > /tmp/stdout.log 2>/tmp/stderr.log
